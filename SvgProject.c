82505819270f6525f93efa570e7e9884002d5ff1
eVerKoZ <everkoz2016@gmail.com>
2025-10-05 18:26:32 +0200

    feat: user can chose many shapes and open the svg file

--- SvgProject.c
+++ SvgProject.c
@@ -6,11 +6,15 @@
 int main() {
         FILE* my_file = fopen("test.svg", "w");
 
+        int i = 0;
+        bool addForme = true;
+        char WantOtherForme[5] = "A";
+
         int sizeViewportStartX = 0;
         int sizeViewportEndX = 0;
         int sizeViewportStartY = 0;
         int sizeViewportEndY = 0;
-
+    
         printf("Rentrer la dimension du tableau\n");
         printf("X de depart : \n");
         scanf("%d", &sizeViewportStartX);
@@ -21,43 +25,101 @@ int main() {
         printf("Y de fin : \n");
         scanf("%d", &sizeViewportEndY);
 
-        typedef enum forme_e {
-            CERCLE,
-            RECTANGLE
-        } forme_et;
+        char formeChose[1000] = "";
+        char sizeViewport[1000];
+        sprintf(sizeViewport, "<svg viewBox='%d %d %d %d' xmlns='http://www.w3.org/2000/svg'>\n", sizeViewportStartX, sizeViewportStartY, sizeViewportEndX, sizeViewportEndY);
+        strcpy(formeChose, sizeViewport);
 
-        forme_et activeForme = CERCLE;
-        char forme[20];
+        char formeToDraw[10000] = "";
 
-        printf("Quelle forme voulez vous dessinez ? : ");
-        scanf("%s",&forme);
-
-        switch (*forme)
+        while (i < 10)
         {
-        case forme == "cercle":
-            activeForme = CERCLE;
-            printf("Vous voulez dessiner un cercle !");
-            break;
-        case forme == "rectangle":
-            activeForme = RECTANGLE;
-            printf("Vous voulez dessiner un rectangle !");
-            break;
-        default:
-            break;
+            if (addForme == true) {
+
+                typedef enum forme_e {
+                    INCONNUE = 0,
+                    CERCLE,
+                    RECTANGLE,
+                    CARRE
+                } forme_et;
+
+                forme_et activeForme;
+                activeForme = RECTANGLE;
+                char forme[20] = "A";
+
+                printf("Quelle forme voulez vous dessinez ? : ");
+                scanf("%s",&forme);
+                printf("%s\n", forme);
+
+                if (strcmp(forme, "cercle") == 0) {
+                    activeForme = CERCLE;
+                } else if (strcmp(forme, "rectangle") == 0) {
+                    activeForme = RECTANGLE;
+                } else if (strcmp(forme, "carre") == 0) {
+                    activeForme = CARRE;
+                } else {
+                    activeForme = INCONNUE;
+                }
+
+                printf("%d\n", activeForme);
+
+                int positionX = 0;
+                int positionY = 0;
+                //char formeChose[1000] = "A";
+
+                switch (activeForme)
+                {
+                case CERCLE:
+                    printf("A quelle position X, Y voulez vous dessinez votre cercle ?\n");
+                    //scanf("Position X : %d, Position Y : %d", &positionX, &positionY);
+                    scanf("%d %d", &positionX, &positionY);
+                    printf("%d, %d\n", positionX, positionY);
+                    printf("Vous avez dessinez un cercle !\n");
+                    char cercle[1000];
+                    sprintf(cercle, "  <circle cx='%d' cy='%d' r='50' fill='green' /> \n", positionX, positionY);
+                    //strcpy(formeChose, cercle);
+                    strcat(formeChose, cercle);
+                    break;
+                case RECTANGLE:
+                    printf("A quelle position X, Y voulez vous dessinez votre rectangle ?\n");
+                    scanf("%d %d", &positionX, &positionY);
+                    printf("%d, %d\n", positionX, positionY);
+                    printf("Vous avez dessinez un rectangle !\n");
+                    char rectangle[1000];
+                    sprintf(rectangle, "  <rect x='%d' y='%d' width='100' height='100' /> \n", positionX, positionY);
+                    strcat(formeChose, rectangle);
+                    break;
+                case CARRE:
+                    printf("Vous avez dessinez un carre !\n");
+                    break;
+                
+                default:
+                    printf("Faites une forme valide !\n");
+                    break;
+                }
+
+                strcpy(formeChose, formeChose);
+                printf("%s", formeChose);
+                strcpy(formeToDraw, formeChose);
+
+
+                printf("Voulez vous ajoutez une autre forme ? ");
+                scanf("%s", &WantOtherForme);
+
+                if (strcmp(WantOtherForme, "oui") == 0)
+                {
+                    addForme = true;
+                } else {
+                    addForme = false;
+                    strcat(formeToDraw, "</svg>");
+                    //printf("%s", formeToDraw);
+                    fprintf(my_file, formeToDraw);
+                    fclose(my_file);
+                    break;
+                }
+            } else {
+                break;
+            }
         }
-        printf("%s", forme);
-
-        char cerle[1000];
-        sprintf(cerle, "<svg viewBox='%d %d %d %d' xmlns='http://www.w3.org/2000/svg'>\n  <circle cx='100' cy='100' r='50' fill='green' /> \n</svg>", sizeViewportStartX, sizeViewportStartY, sizeViewportEndX, sizeViewportEndY);
-        char rectangle[1000];
-        sprintf(rectangle, "<svg viewBox='%d %d %d %d' xmlns='http://www.w3.org/2000/svg'>\n  <rect x='0' y='0' width='100' height='100' /> \n</svg>", sizeViewportStartX, sizeViewportStartY, sizeViewportEndX, sizeViewportEndY);
-
-        printf("%s", cerle);
-        
-        if (fprintf(my_file, cerle) < 0)
-            printf("BOGOSS");
-
-        fclose(my_file);
-
     return 0;
 }
